ea10f7e8a6430ce78df2b5c61a680724a233db99
vrgcstudio@gmail.com <vrgcstudio@gmail.com>
2018-11-24 14:25:19 +0700

    add 2 new page component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Page1Component } from './src/app/components/page1/page1.component';
+import { Page2Component } from './components/page2/page2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Page1Component,
+    Page2Component
   ],
   imports: [
     BrowserModule,
